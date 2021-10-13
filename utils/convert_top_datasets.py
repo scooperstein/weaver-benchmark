830a4156c828ed8db0ae6bbfee1859cd2a341197
Huilin Qu <huilin.qu@cern.ch>
2021-10-13 18:47:46 +0200

    Update config files and add a PFN model.

--- utils/convert_top_datasets.py
+++ utils/convert_top_datasets.py
@@ -1,94 +1,118 @@
+import os
 import pandas as pd
 import numpy as np
-import awkward0 as ak0
-import math
-from tqdm import tqdm
+import awkward0 as awkward
+import uproot3_methods as uproot_methods
 import argparse
-parser = argparse.ArgumentParser('Convert top benchmark h5 datasets to awkd')
-parser.add_argument('-c', '--condition', required=True, help='Create dataset for training/test. Set to \'train\' or \'test\'.')
-parser.add_argument('--max-event-size', default=100000, help='Maximum event size per output file.')
-args = parser.parse_args()
 
-if args.condition == 'train':
-    inputfiles, outputfile = ['train.h5'], 'top_train'
-elif args.condition == 'val':
-    inputfiles, outputfile = ['val.h5'], 'top_val'
-elif args.condition == 'test':
-    inputfiles, outputfile = ['test.h5'], 'top_test'
-else:
-    raise RuntimeError('Invalid --condition option')
 
-def store_file(res_array, outname):
-    print('Saving to file', outname, '...')
-    ak0.save(outname, ak0.fromiter(res_array), mode='w')
- 
-def main():
-    varlist_2d = ['E', 'PX', 'PY', 'PZ']
-    varlist_1d = ['truthE', 'truthPX', 'truthPY', 'truthPZ', 'ttv', 'is_signal_new']
-    varlist_2d_new = ['E_log', 'P', 'P_log', 'Etarel', 'Phirel']
-    varlist_1d_new = ['E_tot', 'PX_tot', 'PY_tot', 'PZ_tot', 'P_tot', 'Eta_tot', 'Phi_tot']
-    idx, ifile = 0, 0
-    res_array = []
-    for filename in inputfiles:
-        print('Reading table from:', filename, '...')
-        with pd.HDFStore(f'samples/{filename}') as store:
-            df = store.select('table')
+import logging
+logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s] %(levelname)s: %(message)s')
 
-        print('Processing events ...')
-        isfirst = True
-        for origIdx, row in tqdm(df.iterrows()):
-            # First initiate 2d arrays
-            res = {k:[] for k in varlist_2d + varlist_2d_new}
-            nPart = 0
-            for ipar in range(200):
-                if row[f'E_{ipar}'] == 0.:
-                    break
-                for k in ['E', 'PX', 'PY', 'PZ']:
-                    res[k].append(row[f'{k}_{ipar}'])
-                res['E_log'].append(math.log(row[f'E_{ipar}']))
-                res['P'].append(math.sqrt(row[f'PX_{ipar}']**2 + row[f'PY_{ipar}']**2 + row[f'PZ_{ipar}']**2))
-                res['P_log'].append(math.log(res['P'][-1]))
-                nPart += 1
-            
-            # Fill 1d arrays
-            for k in varlist_1d:
-                res[k] = row[k]
-            res['E_tot'] = sum(res['E'])
-            res['PX_tot'] = sum(res['PX'])
-            res['PY_tot'] = sum(res['PY'])
-            res['PZ_tot'] = sum(res['PZ'])
-            res['P_tot'] = math.sqrt(res['PX_tot']**2 + res['PY_tot']**2 + res['PZ_tot']**2)
-            res['Eta_tot'] = math.atanh(res['PZ_tot'] / res['P_tot'])
-            res['Phi_tot'] = math.atan(res['PY_tot'] / res['PX_tot'])
-            res['nPart'] = nPart
-            res['origIdx'] = origIdx
-            res['idx'] = idx
-            
-            # Calculate new 2d arrays
-            for ipar in range(nPart):
-                Eta = math.atanh(res['PZ'][ipar] / res['P'][ipar])
-                Phi = math.atan(res['PY'][ipar] / res['PX'][ipar])
-                res['Etarel'].append(np.sign(res['Eta_tot']) * (Eta - res['Eta_tot']))
-                res['Phirel'].append(Phi - res['Phi_tot'] - 2 * math.pi * math.floor((Phi - res['Phi_tot']) / (2 * math.pi) + 0.5))
 
-            # Store per event result
-            res_array.append(res)
-            idx += 1
-                        
-            if isfirst:
-                print(res)
-                isfirst = False
+def _transform(dataframe, start=0, stop=-1):
+    from collections import OrderedDict
+    v = OrderedDict()
 
-            if idx >= args.max_event_size:
-                store_file(res_array, f'{outputfile}_{ifile}.awkd')
-                del res_array
-                res_array = []
-                ifile += 1
-                idx = 0
+    df = dataframe.iloc[start:stop]
 
-    # Save rest of events before finishing
-    store_file(res_array, f'{outputfile}_{ifile}.awkd')
+    def _col_list(prefix, max_particles=200):
+        return ['%s_%d' % (prefix, i) for i in range(max_particles)]
+
+    _px = df[_col_list('PX')].values
+    _py = df[_col_list('PY')].values
+    _pz = df[_col_list('PZ')].values
+    _e = df[_col_list('E')].values
+
+    mask = _e > 0
+    n_particles = np.sum(mask, axis=1)
+
+    px = awkward.JaggedArray.fromcounts(n_particles, _px[mask])
+    py = awkward.JaggedArray.fromcounts(n_particles, _py[mask])
+    pz = awkward.JaggedArray.fromcounts(n_particles, _pz[mask])
+    energy = awkward.JaggedArray.fromcounts(n_particles, _e[mask])
+
+    p4 = uproot_methods.TLorentzVectorArray.from_cartesian(px, py, pz, energy)
+    pt = p4.pt
+
+    jet_p4 = p4.sum()
+
+    # outputs
+    _label = df['is_signal_new'].values
+    v['label'] = _label
+    v['train_val_test'] = df['ttv'].values
+
+    v['jet_px'] = jet_p4.x
+    v['jet_py'] = jet_p4.y
+    v['jet_pz'] = jet_p4.z
+    v['jet_E'] = jet_p4.energy
+
+    v['jet_pt'] = jet_p4.pt
+    v['jet_eta'] = jet_p4.eta
+    v['jet_phi'] = jet_p4.phi
+    v['jet_mass'] = jet_p4.mass
+    v['n_parts'] = n_particles
+
+    v['part_px'] = px
+    v['part_py'] = py
+    v['part_pz'] = pz
+    v['part_energy'] = energy
+
+    v['part_pt_log'] = np.log(pt)
+    v['part_ptrel'] = pt / v['jet_pt']
+    v['part_logptrel'] = np.log(v['part_ptrel'])
+
+    v['part_e_log'] = np.log(energy)
+    v['part_erel'] = energy / jet_p4.energy
+    v['part_logerel'] = np.log(v['part_erel'])
+
+    v['part_raw_etarel'] = (p4.eta - v['jet_eta'])
+    _jet_etasign = np.sign(v['jet_eta'])
+    _jet_etasign[_jet_etasign == 0] = 1
+    v['part_etarel'] = v['part_raw_etarel'] * _jet_etasign
+
+    v['part_phirel'] = p4.delta_phi(jet_p4)
+    v['part_deltaR'] = np.hypot(v['part_etarel'], v['part_phirel'])
+
+    return v
+
+
+def convert(source, destdir, basename, step=None, limit=None):
+    df = pd.read_hdf(source, key='table')
+    logging.info('Total events: %s' % str(df.shape[0]))
+    if limit is not None:
+        df = df.iloc[0:limit]
+        logging.info('Restricting to the first %s events:' % str(df.shape[0]))
+    if step is None:
+        step = df.shape[0]
+    idx = -1
+    while True:
+        idx += 1
+        start = idx * step
+        if start >= df.shape[0]:
+            break
+        if not os.path.exists(destdir):
+            os.makedirs(destdir)
+        output = os.path.join(destdir, '%s_%d.awkd' % (basename, idx))
+        logging.info(output)
+        if os.path.exists(output):
+            os.remove(output)
+        v = _transform(df, start=start, stop=start + step)
+        awkward.save(output, v, mode='x')
 
 
 if __name__ == '__main__':
-    main()
+
+    parser = argparse.ArgumentParser('Convert top benchmark h5 datasets to awkd')
+    parser.add_argument('-i', '--inputdir', required=True, help='Directory of input h5 files.')
+    parser.add_argument('-o', '--outputdir', required=True, help='Output directory.')
+    args = parser.parse_args()
+
+    # conver training file
+    convert(os.path.join(args.inputdir, 'train.h5'), destdir=args.outputdir, basename='train_file')
+
+    # conver validation file
+    convert(os.path.join(args.inputdir, 'val.h5'), destdir=args.outputdir, basename='val_file')
+
+    # conver testing file
+    convert(os.path.join(args.inputdir, 'test.h5'), destdir=args.outputdir, basename='test_file')
