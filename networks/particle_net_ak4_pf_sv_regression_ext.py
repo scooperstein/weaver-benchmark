d20d2664368a12093909749b090c9c3d87999e08
Raffaele <raffaele.gerosa@cern.ch>
2022-05-09 19:40:09 +0200

    updates

---
+++ networks/particle_net_ak4_pf_sv_regression_ext.py
@@ -0,0 +1,67 @@
+import numpy as np
+import math
+import torch
+from torch import Tensor
+from utils.nn.model.ParticleNet import ParticleNetTagger
+
+
+def get_model(data_config, **kwargs):
+    conv_params = [
+        (16, (160, 128, 96)),
+        (12, (192, 160, 128)),
+        (8,  (192, 160, 128)),
+    ]
+    fc_params = [
+        (192, 0.1),
+        (160, 0.1),
+        (128, 0.1),
+        (96, 0.1),
+        (64, 0.1)
+    ]
+    use_fusion = True
+
+    pf_features_dims = len(data_config.input_dicts['pf_features'])
+    sv_features_dims = len(data_config.input_dicts['sv_features'])
+    num_classes = 0
+    num_targets = 1
+    model = ParticleNetTagger(pf_features_dims, 
+                              sv_features_dims, 
+                              num_classes,
+                              conv_params, 
+                              fc_params,
+                              input_dims=48,
+                              use_fusion=use_fusion,
+                              use_fts_bn=kwargs.get('use_fts_bn', False),
+                              use_counts=kwargs.get('use_counts', True),
+                              pf_input_dropout=kwargs.get('pf_input_dropout', None),
+                              sv_input_dropout=kwargs.get('sv_input_dropout', None),
+                              for_inference=kwargs.get('for_inference', False)
+                              )
+
+    model_info = {
+        'input_names':list(data_config.input_names),
+        'input_shapes':{k:((1,) + s[1:]) for k, s in data_config.input_shapes.items()},
+        'output_names':['output'],
+        'dynamic_axes':{**{k:{0:'N', 2:'n_' + k.split('_')[0]} for k in data_config.input_names}, **{'output':{0:'N'}}},
+        }
+
+    return model, model_info
+
+class LogCoshLoss(torch.nn.L1Loss):
+    __constants__ = ['reduction']
+
+    def __init__(self, reduction: str = 'mean') -> None:
+        super(LogCoshLoss, self).__init__(None, None, reduction)
+
+    def forward(self, input: Tensor, target: Tensor) -> Tensor:
+        x = input - target
+        loss = x + torch.nn.functional.softplus(-2. * x) - math.log(2)
+        if self.reduction == 'none':
+            return loss
+        elif self.reduction == 'mean':
+            return loss.mean()
+        elif self.reduction == 'sum':
+            return loss.sum()
+
+def get_loss(data_config, **kwargs):
+    return LogCoshLoss(reduction='mean');
