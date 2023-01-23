2428a298d2da79e80af155f92578583eaa9ebfd7
Raffaele <raffaele.gerosa@cern.ch>
2023-01-23 20:40:16 +0100

    adding puppi yaml files for ak4 jets

--- networks/particle_net_ak4_pf_sv_class_reg_domain_simpler.py
+++ networks/particle_net_ak4_pf_sv_class_reg_domain_simpler.py
@@ -16,29 +16,39 @@ def get_model(data_config, **kwargs):
         ]
     ## use fusion layer for edge-conv block
     use_fusion = True
-    ## fully connected output layers
+    ## fully connected output layers                                                                                                                                                                  
     fc_params = [
-        (224, 0.1),
-        (192, 0.1),
-        (160, 0.1),
+        (256, 0.1),
         (128, 0.1),
-        (96,  0.1),
+        (96, 0.1),
         (64,  0.1)
     ]
-    ## fully connected output layers
+    ## fully connected output layers                                                                                                                                                                  
     fc_domain_params = [
-        (160, 0.1),
-        (96, 0.1),
+        (256, 0.1),
+        (128, 0.1),
+        (64,  0.1)
         (32,  0.1)
     ]
+
 
     ## classes and features
     pf_features_dims = len(data_config.input_dicts['pf_features'])
     sv_features_dims = len(data_config.input_dicts['sv_features'])
     lt_features_dims = len(data_config.input_dicts['lt_features'])
+
+    ## number of classes for the multi-class loss
     num_classes = len(data_config.label_value);
-    num_targets = len(data_config.target_value)
-    num_domains = len(data_config.label_domain_value)
+    ## number of targets
+    if type(data_config.target_value) == dict:
+        num_targets = sum(len(dct) if type(dct) == list else 1 for dct in data_config.target_value.values())
+    else:
+        num_targets = len(data_config.target_value);
+    ## number of domain labels in the various regions (one binary or multiclass per region)
+    if type(data_config.label_domain_value) == dict:
+        num_domains = sum(len(dct) if type(dct) == list else 1 for dct in data_config.label_domain_value.values())
+    else:
+        num_domains = len(data_config.label_domain_value);
 
     model = ParticleNetLostTrkTagger(pf_features_dims=pf_features_dims, 
                                      sv_features_dims=sv_features_dims, 
@@ -72,86 +82,106 @@ def get_model(data_config, **kwargs):
 
 
 class CrossEntropyLogCoshLossDomain(torch.nn.L1Loss):
-    __constants__ = ['reduction','loss_lambda','loss_gamma','quantiles','loss_kappa']
+    __constants__ = ['reduction','loss_lambda','loss_gamma','quantiles','loss_kappa','domain_weight','domain_dim']
 
     def __init__(self, 
                  reduction: str = 'mean', 
                  loss_lambda: float = 1., 
                  loss_gamma: float = 1., 
                  loss_kappa: float = 1., 
-                 quantiles: list = []) -> None:
+                 quantiles: list = [],
+                 domain_weight: list = [],
+                 domain_dim: list = [],
+             ) -> None:
         super(CrossEntropyLogCoshLossDomain, self).__init__(None, None, reduction)
         self.loss_lambda = loss_lambda;
         self.loss_gamma = loss_gamma;
         self.loss_kappa = loss_kappa;
         self.quantiles = quantiles;
+        self.domain_weight = domain_weight;
+        self.domain_dim = domain_dim;
 
     def forward(self, 
                 input_cat: Tensor, y_cat: Tensor, 
                 input_reg: Tensor, y_reg: Tensor, 
-                input_domain: Tensor, y_domain: Tensor) -> Tensor:
+                input_domain: Tensor, y_domain: Tensor, y_domain_check: Tensor) -> Tensor:
 
         ## classification term
-        input_cat = input_cat.squeeze();
-        y_cat     = y_cat.squeeze().long();
-        loss_cat  = torch.nn.functional.cross_entropy(input_cat,y_cat,reduction=self.reduction);
+        input_cat  = input_cat.squeeze();
+        y_cat      = y_cat.squeeze().long();
+        loss_cat   = 0;
+
+        if input_cat.nelement():
+            loss_cat = torch.nn.functional.cross_entropy(input_cat,y_cat,reduction=self.reduction);
 
         ## regression terms
         input_reg  = input_reg.squeeze();
         y_reg      = y_reg.squeeze();
-        x_reg      = input_reg-y_reg;
-        
-        loss_mean  = None;
-        loss_quant = None;
+        x_reg      = input_reg-y_reg;        
+        loss_mean  = 0;
+        loss_quant = 0;
+        loss_reg   = 0;
 
-        for idx,q in enumerate(self.quantiles):
-            if q <= 0 and loss_mean is None:
-                loss_mean  = (x_reg[:,idx])+torch.nn.functional.softplus(-2.*(x_reg[:,idx]))-math.log(2);
-            elif q <= 0 and loss_mean is not None:
-                loss_mean  = loss_mean + (x_reg[:,idx])+torch.nn.functional.softplus(-2.*(x_reg[:,idx]))-math.log(2);
-            if q > 0 and loss_quant is None:
-                loss_quant = q*x_reg[:,idx]*torch.ge(x_reg[:,idx],0)
-                loss_quant = loss_quant + (q-1)*(x_reg[:,idx])*torch.less(x_reg[:,idx],0);
-            elif q > 0 and loss_quant is not None:
-                loss_quant = loss_quant + q*x_reg[:,idx]*torch.ge(x_reg[:,idx],0)
-                loss_quant = loss_quant + (q-1)*(x_reg[:,idx])*torch.less(x_reg[:,idx],0);                
-
-        if self.reduction == 'mean':
-            if loss_quant is not None:
+        if input_reg.nelement():
+            ## compute loss
+            for idx,q in enumerate(self.quantiles):
+                if q <= 0:
+                    loss_mean += (x_reg[:,idx])+torch.nn.functional.softplus(-2.*(x_reg[:,idx]))-math.log(2);
+                if q > 0:
+                    loss_quant += q*x_reg[:,idx]*torch.ge(x_reg[:,idx],0)
+                    loss_quant += (q-1)*(x_reg[:,idx])*torch.less(x_reg[:,idx],0);
+            ## reduction
+            if self.reduction == 'mean':
                 loss_quant = loss_quant.mean();
-            if loss_mean is not None:
                 loss_mean = loss_mean.mean();
-        elif self.reduction == 'sum':
-            if loss_quant is not None:
+            elif self.reduction == 'sum':
                 loss_quant = loss_quant.sum();
-            if loss_mean is not None:
                 loss_mean = loss_mean.sum();
-            
-        if loss_quant is not None and loss_mean is not None:
+            ## composition
             loss_reg = self.loss_lambda*loss_mean+self.loss_gamma*loss_quant;
-        elif loss_mean is not None:
-            loss_reg = self.loss_lambda*loss_mean;
-        elif loss_quant is not None:
-            loss_reg = self.loss_gamma*loss_quant;
 
         ## domain terms
-        input_domain  = input_domain.squeeze();
-        y_domain      = y_domain.squeeze();
+        input_domain   = input_domain.squeeze();
+        y_domain       = y_domain.squeeze();
+        y_domain_check = y_domain_check.squeeze();
+        loss_domain    = 0;
+
         if input_domain.nelement():
-            loss_domain = self.loss_kappa*torch.nn.functional.cross_entropy(input_domain,y_domain,reduction=self.reduction);
-        else:
-            loss_domain = torch.tensor([0.0],requires_grad=loss_cat.requires_grad)
-        
+            ## just one domain region
+            if not self.domain_weight:
+                loss_domain = self.loss_kappa*torch.nn.functional.cross_entropy(input_domain,y_domain,reduction=self.reduction);
+            else:
+                ## more domain regions with different relative weights
+                for id,w in enumerate(self.domain_weight):
+                    id_dom  = id*self.domain_dim[id];
+                    y_check = y_domain_check[:,id]
+                    indexes = y_check.nonzero();                    
+                    y_val   = input_domain[indexes,id_dom:id_dom+self.domain_dim[id]].squeeze();
+                    y_pred  = y_domain[indexes,id].squeeze();
+                    if y_val.nelement():
+                        loss_domain += w*torch.nn.functional.cross_entropy(y_val,y_pred,reduction=self.reduction);
+                loss_domain *= self.loss_kappa;
+            
         return loss_cat+loss_reg+loss_domain, loss_cat, loss_reg, loss_domain;
 
 def get_loss(data_config, **kwargs):
 
+    ## number of targets
     quantiles = data_config.target_quantile;
+    ## number of domain regions
+    wdomain = data_config.label_domain_loss_weight;
+    ## number of lables for cross entropy in each domain
+    if type(data_config.label_domain_value) == dict:
+        ldomain = [len(dct) if type(dct) == list else 1 for dct in data_config.label_domain_value.values()]
+    else:
+        ldomain = [len(data_config.label_domain_value)];
 
     return CrossEntropyLogCoshLossDomain(
         reduction=kwargs.get('reduction','mean'),
         loss_lambda=kwargs.get('loss_lambda',1),
         loss_gamma=kwargs.get('loss_gamma',1),
         loss_kappa=kwargs.get('loss_kappa',1),
-        quantiles=quantiles
+        quantiles=quantiles,
+        domain_weight=wdomain,
+        domain_dim=ldomain
     );
