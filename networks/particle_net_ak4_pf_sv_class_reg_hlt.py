954403c80d0fdb91cd29088f7ffc430f914ba779
Stephane Cooperstein <stephane.b.cooperstein@cern.ch>
2023-03-09 15:41:57 +0100

    fix import

--- networks/particle_net_ak4_pf_sv_class_reg_hlt.py
+++ networks/particle_net_ak4_pf_sv_class_reg_hlt.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 import torch
 from torch import Tensor
-from nn.model.ParticleNet import ParticleNetLostTrkTagger
+from nn.model.ParticleNet import ParticleNetTagger
 
 def get_model(data_config, **kwargs):
 
