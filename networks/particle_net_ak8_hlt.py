ede9485a746dd54bbb62f1ad465a652ae8ea0ab3
Stephane Cooperstein <stephane.b.cooperstein@cern.ch>
2023-03-23 09:35:46 +0100

    no targets for AK8 HLT

--- networks/particle_net_ak8_hlt.py
+++ networks/particle_net_ak8_hlt.py
@@ -27,7 +27,7 @@ def get_model(data_config, **kwargs):
     pf_features_dims = len(data_config.input_dicts['pf_features'])
     sv_features_dims = len(data_config.input_dicts['sv_features'])
     num_classes = len(data_config.label_value);
-    num_targets = len(data_config.target_value)
+    num_targets = 0
 
     model = ParticleNetTagger(pf_features_dims=pf_features_dims, 
                                      sv_features_dims=sv_features_dims, 
